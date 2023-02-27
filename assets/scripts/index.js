a624c8e997c3500b818f64605a88bd6b6b2bd648
laubg <laurabautistg@gmail.com>
2023-02-27 11:57:01 -0300

    funcionando index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -18,14 +18,25 @@ console.log(data.events);
 
 let newCards = crearCards(data.events)
 
-
 contenedorCards.innerHTML = newCards
 
+
 function crearCards(arrayDatos){
-    let tarjetas =''
+    let tarjetas =""
 
     for (const tarjeta of arrayDatos) {      
     
-    tarjetas += '<div class="card col-sm-6 col-md-4 " ><img src="/assets/img/Cinema.jpg"class="card-img-top" alt="cinema"><div class="card-body">                            <h5 class="card-title">Card title</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>                         <div class="row card-row">                               <div class="precio col-md-6 col-sm-12"> <h6>$XXXXX</h6></div>                                <div class="botón col-md-6 col-sm-12"><a href="details.html" class="btn btn-primary">Details</a></div>                            </div>                    </div>                    </div>'
-                } return tarjetas
+    tarjetas += `<div class="card col-sm-6 col-md-4 " >        
+                    <img src= ${tarjeta.image}  class="card-img-top" alt="cinema">    
+                    <div class="card-body">  
+                        <h5 class="card-title">${tarjeta.name}</h5>
+                        <p class="card-text">${tarjeta.description}</p>                         
+                        <div class="row card-row">                               
+                            <div class="precio col-md-6 col-sm-12"> <h6>   $${tarjeta.price}</h6></div>                                
+                            <div class="botón col-md-6 col-sm-12"><a href="details.html" class="btn btn-primary">Details</a></div>                        
+                        </div>                   
+                    </div>                    
+                </div>               `   } 
+                return tarjetas
 }
+// ``
