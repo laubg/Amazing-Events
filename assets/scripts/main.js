a574c97f703cb465996abb42d12b4b8954f361f9
laubg <laurabautistg@gmail.com>
2023-03-02 20:45:31 -0300

    for a foreach en el main

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -16,27 +16,58 @@
 const contenedorCards= document.querySelector('.cards')
 console.log(data.events);
 
+
+
+
+
+
+// function crearCards(arrayEventos){
+//     let tarjetas =""
+    
+
+//     for (const tarjeta of arrayEventos) {     
+        
+        
+//         tarjetas += `<div class="card col-sm-6 col-md-4 " >        
+//                     <img src= ${tarjeta.image}  class="card-img-top" alt="cinema" >    
+//                     <div class="card-body style="heigth:auto"">  
+//                         <h5 class="card-title">${tarjeta.name}</h5>
+//                         <p class="card-text">${tarjeta.description}</p>                         
+//                         <div class="row card-row">                               
+//                             <div class="precio col-md-6 col-sm-12"> <h6>   $${tarjeta.price}</h6></div>                                
+//                             <div class="botón col-md-6 col-sm-12"><a href="details.html" class="btn btn-primary">Details</a></div>                        
+//                         </div>                   
+//                     </div>                    
+//                 </div>               `   }  
+//                 return tarjetas
+
+// }
 let newCards = crearCards(data.events)
-
+               
 contenedorCards.innerHTML = newCards
-
-
+   
 function crearCards(arrayEventos){
     let tarjetas =""
-
-    for (const tarjeta of arrayEventos) {      
     
-    tarjetas += `<div class="card col-sm-6 col-md-4 " >        
-                    <img src= ${tarjeta.image}  class="card-img-top" alt="cinema" >    
-                    <div class="card-body style="heigth:auto"">  
-                        <h5 class="card-title">${tarjeta.name}</h5>
-                        <p class="card-text">${tarjeta.description}</p>                         
-                        <div class="row card-row">                               
-                            <div class="precio col-md-6 col-sm-12"> <h6>   $${tarjeta.price}</h6></div>                                
-                            <div class="botón col-md-6 col-sm-12"><a href="details.html" class="btn btn-primary">Details</a></div>                        
-                        </div>                   
-                    </div>                    
-                </div>               `   } 
-                return tarjetas
-}
+
+    arrayEventos.forEach(evento => {
+        
+        tarjetas += `<div class="card col-sm-6 col-md-4 " >        
+        <img src= ${evento.image}  class="card-img-top" alt="cinema" >    
+        <div class="card-body style="heigth:auto"">  
+            <h5 class="card-title">${evento.name}</h5>
+            <p class="card-text">${evento.description}</p>                         
+            <div class="row card-row">                               
+                <div class="precio col-md-6 col-sm-12"> <h6>   $${evento.price}</h6></div>                                
+                <div class="botón col-md-6 col-sm-12"><a href="details.html" class="btn btn-primary">Details</a></div>                        
+            </div>                   
+        </div>                    
+    </div>               `
+    }  
+    );
+    return tarjetas
+}    
+
+
 // ``
+
