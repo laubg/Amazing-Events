c2b4027a30f73c69e9bd57c414f50f82de92d026
laubg <laurabautistg@gmail.com>
2023-03-07 17:47:31 -0300

    listo responsive de details!

--- assets/scripts/details.js
+++ assets/scripts/details.js
@@ -14,28 +14,57 @@ let evento = data.events.find(evento => evento._id == id);
 
 let templateHtml = "";
 
-templateHtml = `   <div class="card col-9 " style="background-color: "black"" >
-                <img src="${evento.img}">
-            <div class="card-body">
-                <h1 class="card-title">${evento.name}</h1>
-                <p class="card-text">${evento.description}</p>  
-                <div class="row card-row">
-                    <div class="datos col-md-6 col-sm-12"> <h5>Date: ${evento.date}</h5></div>
-                    <div class="derecha col-md-6 col-sm-12"><h5>Location: ${evento.place}</h5></div>
-                </div> 
-                <div class="row card-row">
-                    <div class="datos col-md-6 col-sm-12"> <h5>Capacity: ${evento.capacity}</h5></div>
-                    <div class="derecha col-md-6 col-sm-12"><h5>Assistance:${evento.assistance}</h5></div>
-                </div>
-                <div class="row card-row">
-                    <div class="datos col-md-6 col-sm-12"> <h5>$${evento.price}</h5></div>
-                    
-                </div>
-                <div class="botón col-md-12 col-sm-12"><a href="#" class="btn-buy btn btn-primary">Buy</a></div>
-            </div>
-        </div>
+templateHtml = `<section class="card col-10"> 
+    
+                    <div class="col-5 "  >
+                       <img src="${evento.image}" class="img-fluid">
+                    </div> 
+                    <div class="card-body col-4 ">
+                        <h1 class="card-title">${evento.name}</h1>
+                        <p class="card-text">${evento.description}</p>  
+                        <div class="row card-row">
+                            <div class="datos col-md-6 col-sm-12"> <h5>Date: ${evento.date}</h5></div>
+                            <div class="derecha col-md-6 col-sm-12"><h5>Location: ${evento.place}</h5></div>
+                        </div> 
+                        <div class="row card-row">
+                            <div class="datos col-md-6 col-sm-12"> <h5>Capacity: ${evento.capacity}</h5></div>
+                            <div class="derecha col-md-6 col-sm-12"><h5>Assistance: ${evento.assistance}</h5></div>
+                        </div>
+                        <div class="row card-row">
+                            <div class="datos2 col-md-6 col-sm-12"> <h6>Price: $${evento.price}</h6></div>
+                        </div>
+                        <div class="botón col-md-12 col-sm-12"><a href="#" class="btn-buy btn btn-primary">Buy</a></div>
+                        </div>
+                    </div>
+                </section>  
     `
 console.log(templateHtml);
 
-    document.getElementsByClassName("búsqueda").innerHTML= templateHtml;
+document.querySelector(".imprimir").innerHTML= templateHtml;
+
+//     function mostrarEvento(container,evento){
+//         container.innerHTML=`   <div class="card col-9 " style="background-color: "black"" >
+//         <img src="${evento.img}">
+//     <div class="card-body">
+//         <h1 class="card-title">${evento.name}</h1>
+//         <p class="card-text">${evento.description}</p>  
+//         <div class="row card-row">
+//             <div class="datos col-md-6 col-sm-12"> <h5>Date: ${evento.date}</h5></div>
+//             <div class="derecha col-md-6 col-sm-12"><h5>Location: ${evento.place}</h5></div>
+//         </div> 
+//         <div class="row card-row">
+//             <div class="datos col-md-6 col-sm-12"> <h5>Capacity: ${evento.capacity}</h5></div>
+//             <div class="derecha col-md-6 col-sm-12"><h5>Assistance:${evento.assistance}</h5></div>
+//         </div>
+//         <div class="row card-row">
+//             <div class="datos col-md-6 col-sm-12"> <h5>$${evento.price}</h5></div>
+            
+//         </div>
+//         <div class="botón col-md-12 col-sm-12"><a href="#" class="btn-buy btn btn-primary">Buy</a></div>
+//     </div>
+// </div>
+// `
+//     }
+// let contenedor= document.querySelector(".imprimir")
+// mostrarEvento(contenedor,evento)
 
